76d9b0be93962f7708d7ba04c3e80cff81c6121d
Velxy <39607289+Velxy@users.noreply.github.com>
2022-01-12 23:07:33 +0900

    코드 수정

--- Programmers/Sungho/dualFucQue.py
+++ Programmers/Sungho/dualFucQue.py
@@ -2,16 +2,13 @@ def solution(operations):
     que = []
 
     for i in operations:
-        print(que)
         if i == "D 1":
             try:
-                print("max: ", max(que))
                 que.remove(max(que))
             except:
                 print("D1 err")
         elif i == "D -1":
             try:
-                print("min: ", min(que))
                 que.remove(min(que))
             except:
                 print("D-1 err")
@@ -24,7 +21,3 @@ def solution(operations):
     else:
         que = [max(que), min(que)]
     return que
-
-print(solution(["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"]))
-list = [-45, -642]
-print(max(list), min(list))

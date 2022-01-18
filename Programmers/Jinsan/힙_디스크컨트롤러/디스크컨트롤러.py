cbbc157d832b4ee5dde2dde0440619862ed3536c
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2022-01-18 16:16:45 +0900

    [Programmers-힙] 디스크_컨트롤러_pr수정

--- Programmers/Jinsan/힙_디스크컨트롤러/디스크컨트롤러.py
+++ Programmers/Jinsan/힙_디스크컨트롤러/디스크컨트롤러.py
@@ -2,20 +2,49 @@
 
 #프로그래머스_정렬_H-index
 
-#citations = [3, 0, 6, 1, 5]
-citations = [9, 7, 6, 2, 1]
+jobs = [[0, 3], [1, 9], [2, 6]]
 
-def solution(citations):
+import heapq as hq
+
+def solution(jobs):
     answer = 0
     
-    citations.sort(reverse = True) #내림차순 정렬
+    jobs_len = len(jobs)
     
-    for i in range(len(citations)):
-        #리스트의 i번째 값이 i+1 이상일 경우
-        #요구 조건에 부합
-        if citations[i] >= i+1:
-            answer = i+1
+    hq.heapify(jobs) #힙 이용해 jobs 정렬
     
+    time = 0
+    now_task_finish_time = 0 #현재 작업의 종료 예정 시간
+    now_task = [0, 0] #현재 진행중인 작업
+    wait_task = [] #대기중인 작업 목록
+    task_time_list = [] #각 요청부터 종료까지의 시간 목록
+    
+    while(True):
+        if len(jobs) > 0 and min(jobs)[0] <= time: #지금 시간에 요청 들어온 작업이 있는 경우
+                #min으로 받아오는 이유는 현재 진행과 대기 중인 작업이 없을 경우엔
+                #가장 요청이 빠른 작업을 받아 오기 위함
+                tmp = min(jobs)
+                idx = jobs.index(min(jobs))
+                jobs.pop(idx)
+                tmp.reverse()#wait_task를 힙으로 정렬해 최소값을 가져오기 위해 순서 바꿈
+                hq.heappush(wait_task, tmp)
+        elif len(jobs) == 0 and len(wait_task) == 0:
+            break
+
+                
+        if time >= now_task_finish_time: #진행중이던 작업이 끝난 경우
+            if len(wait_task) >= 1: #대기중인 작업 1개 이상
+                now_task = hq.heappop(wait_task) #소요시간 최소 작업을 가져옴
+                now_task_finish_time = now_task[0] + time
+                task_time_list.append(now_task_finish_time - now_task[1])
+                time += 1
+            elif len(wait_task) < 1: #대기중인 작업 X
+                time += 1
+                       
+        else: #아직 작업중일때
+            time += 1
+
+    answer = int(sum(task_time_list) / jobs_len)
     return answer
 
-solution(citations)
+solution(jobs)

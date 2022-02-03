a818d9ebfdd936fc77d34a8b7c01d8b03245aa1c
ParkSungHyun <psh1030.dev@gmail.com>
2022-02-03 10:38:47 +0900

    7주차 DFS/BFS 단어변환

--- Programmers/Sunghyeon/6주차 동적 계획법/등굣길.py
+++ Programmers/Sunghyeon/6주차 동적 계획법/등굣길.py
@@ -2,18 +2,18 @@ def solution(m, n, puddles):
     arr=[[0]*m for _ in range(n)]
     arr[0][0]=1;
     for i in range(0,len(puddles)):
-        arr[puddles[i][1]-1][puddles[i][0]-1]=-1;
+        arr[puddles[i][1]-1][puddles[i][0]-1]=-1; ## 물가를표시
     for i in range(0,n):
         for j in range(0,m):
-            if(arr[i][j]==-1):
+            if(arr[i][j]==-1): ##물가일경우 0으로 바꾸어줌
                 arr[i][j]==0
             else:
-                if(arr[i-1][j]==-1):
+                if(arr[i-1][j]==-1): ## 물가가 주위에 있을경우를 생각하여 다르게 계산함
                     arr[i][j]+=arr[i][j-1];
                 elif(arr[i][j-1]==-1):
                     arr[i][j]+=arr[i-1][j];
                 else:
-                    arr[i][j]+=arr[i][j-1]+arr[i-1][j];
+                    arr[i][j]+=arr[i][j-1]+arr[i-1][j]; 
     return arr[n-1][m-1]%1000000007;
                 
         

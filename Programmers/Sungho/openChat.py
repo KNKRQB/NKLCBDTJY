3543aba06c02e0786372ecaffc4743ea713aac4d
Velxy <39607289+Velxy@users.noreply.github.com>
2022-04-14 22:38:02 +0900

    오픈채팅방!

--- Programmers/Sungho/openChat.py
+++ Programmers/Sungho/openChat.py
@@ -1,4 +1,4 @@
-def solution(record):
+def solution1(record):
     #0  Enter uid name
     #1  Leave uid
     #2  Change uid name
@@ -27,11 +27,14 @@ def solution(record):
             users[1][users[0].index(uid)] = rec.split()[2]
     ans = []
     temp = dict()
+
     #uid: name
     for uid, order in zip(priOut[0],priOut[1]):
+
         if order == -1:
             continue
         print(temp, ans)
+
         if uid in temp:
             name = temp[uid]
         else:
@@ -45,7 +48,59 @@ def solution(record):
         else:
             continue
         ans.append(tt)
+
     print(tt)
     return ans
+#======================================================================================
+def solution2(record):
+    #0  Enter uid name
+    #1  Leave uid
+    #2  Change uid name
 
-solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"])
+
+    users = {}
+    #users = {uid : name, ...}
+    #users = [[uid1, uid2, uid3, ...] , [name1, name2, ...]]
+    priOut = [[-1], [-1]]
+    #priOut = [[uid1, uid2, ...] , [order1, order2, ...]]
+
+
+    for rec in record:
+        order = rec.split()[0]
+        uid = rec.split()[1]
+
+        if(order == "Enter"):
+            users[uid] = rec.split()[2]
+
+            priOut[0].append(uid)
+            priOut[1].append(0)
+
+        elif(order == "Leave"):
+            priOut[0].append(uid)
+            priOut[1].append(1)
+        else:
+            users[uid] = rec.split()[2]
+
+    ans = []
+    temp = dict()
+    #uid: name
+    for uid, order in zip(priOut[0], priOut[1]):
+        if order == -1:
+            continue
+
+        if uid in temp:
+            name = temp[uid]
+        else:
+            name = users[uid]
+            temp[uid] = name
+
+        if order == 0:
+            tt = name + "님이 들어왔습니다."
+        elif order == 1:
+            tt = name + "님이 나갔습니다."
+        else:
+            continue
+        ans.append(tt)
+    return ans
+
+solution2(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"])

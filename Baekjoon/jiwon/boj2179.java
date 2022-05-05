163e1dbe338f75eac8300bb5c4bdb50376363f08
xx10222 <yjw4960@gmail.com>
2022-05-06 04:57:46 +0900

    boh 2179 - 비슷한 단어.java

---
+++ Baekjoon/jiwon/boj2179.java
@@ -0,0 +1,48 @@
+package jiwon;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class boj2179 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        List<String> arr = new ArrayList<>();
+
+        for(int i=0;i<n;i++) {
+            String str = scanner.next();
+            if(!arr.contains(str)) {
+                arr.add(str);
+            }
+        }
+
+        int a = 0;
+        int b = 0;
+        int max = 0;
+
+        for(int i=0;i<n-1;i++) {
+            String str1 = arr.get(i);
+            for(int j=i+1;j<n;j++) {
+                String str2 = arr.get(j);
+                int count = 0;
+                int len = (str1.length()<str2.length()) ? str1.length() : str2.length();
+                for(int k=0;k<len;k++) {
+                    if(str1.charAt(k)!=str2.charAt(k)) {
+                        break;
+                    }
+                    count++;
+                }
+                if(max<count) {
+                    max = count;
+                    a = i;
+                    b = j;
+                }
+            }
+        }
+
+        System.out.println(arr.get(a));
+        System.out.println(arr.get(b));
+
+    }
+}
